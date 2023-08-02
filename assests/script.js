531dd35be64ca098280529f770617ee676cff9f6
Molly Starnes <mstrnes2@gmail.com>
2023-08-02 15:16:29 -0500

    added the colors to the different time blocks with if statement

--- assests/script.js
+++ assests/script.js
@@ -9,15 +9,53 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  var saveBtn = $(".saveBtn");
+
+  saveBtn.on("click", function(event) {
+    event.preventDefault();
+    var text = $("description").val();
+
+    localStorage.setItem("text", text);
+  })
+
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  function getTime(){
+    var currentHour = dayjs().hour();
+    console.log(currentHour);
+    $(".time-block").each(function () {
+      var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+      if (timeBlock < currentHour) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      }
+      else if (timeBlock > currentHour) {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+      else {
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      }
+    })
+  }
+
+  getTime();
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  let today = dayjs();
+  $('#currentDay').text(today.format("dddd, MMMM D, YYYY"));
+
 });
